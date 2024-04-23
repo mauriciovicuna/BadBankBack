5a416ba355c68941b8e680f4d1a11bf6f1c95a71
Mauricio <mauricio30042003@gmail.com>
2024-04-22 19:00:43 -0500

    login with google

--- public/login.js
+++ public/login.js
@@ -1,18 +1,34 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBWkRoYgXQr_JzdEpCbEauuY6-1pcmnMng",
+  authDomain: "full-stack-banking-app-bd675.firebaseapp.com",
+  projectId: "full-stack-banking-app-bd675",
+  storageBucket: "full-stack-banking-app-bd675.appspot.com",
+  messagingSenderId: "495563217108",
+  appId: "1:495563217108:web:0b292df9107386c67fc341",
+  measurementId: "G-SB3JSC1NYY"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+
+
+
 function Login(){
   const [show, setShow]     = React.useState(true);
   const [status, setStatus] = React.useState('');    
   const ctx                 = React.useContext(UserContext); 
   const [logged,setLogged]      = React.useState({name: ctx.user.name, email: ctx.user.email});
-  const mystyle = {float:"right", width: "50%"}
 
   React.useEffect(()=>{
-    ctx.user = {name: logged.name, email: logged.email}
+    ctx.user = {name: logged.name, email: logged.email};
+    const span = document.getElementById("name");
     if(ctx.user.name != ''){
     setShow(false)
-    const span = document.getElementById("name");
-    span.innerHTML = `<strong>Hi there ${logged.name}!</strong>`
+    span.innerHTML = `Hi there<strong> ${logged.name}!</strong>`
     }
+    else {span.innerHTML = ``}
   },[logged])
   return (
     <>
@@ -32,6 +48,11 @@ function LoginMsg(props){
   function handleLogout(){
     props.setShow(true)
     props.setLogged({name:'',email:''})
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
     
   }
   return(<>
@@ -39,7 +60,7 @@ function LoginMsg(props){
     <button type="submit" 
       className="btn btn-light" 
       onClick={handleLogout}>
-        Authenticate again
+        Log out
     </button>
   </>);
 }
@@ -58,7 +79,7 @@ function LoginForm(props){
             console.log('JSON:', data);    
             props.setLogged(data); 
         } catch(err) {
-            props.setStatus(text);
+            props.setStatus("not a valid user");
 
             console.log('err:', text);
             
@@ -67,7 +88,30 @@ function LoginForm(props){
 
 
   }
+  function handleOAuth(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+    .auth()
+    .signInWithPopup( provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // The signed-in user info.
 
+      const user = result.user;
+      console.log(user);
+      console.log(user.displayName +" " + user.email);
+      props.setLogged({name:user.displayName, email:user.email})
+      
+      
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      console.log("errors:" + errorCode)
+      // ...
+  });
+  }
 
 
   return (<>
@@ -87,6 +131,7 @@ function LoginForm(props){
       onChange={e => setPassword(e.currentTarget.value)}/><br/>
 
     <button type="submit" className="btn btn-light" onClick={handle}>Login</button>
+    <button type= "submit" className="btn btn-light" onClick={handleOAuth}>Login with OAuth</button>
    
   </>);
 }
